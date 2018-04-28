cf53c992e74e42054ca167232eadf1c139327245
Yohei Kusano <nioh@live.jp>
2018-04-28 18:17:42 +0900

    bit

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,43 +1,48 @@
 import LastFM from 'last-fm'
 
 browser.tabs.query({
-    currentWindow:true,
-    active: true
-}, function (tab) {
-    browser.tabs.sendMessage(tab[0].id, {
-        text: ''
-    }).then(function (response) {
-        const title = response.title
-        const str = response.str
-        document.getElementById('title').innerText = `you have selected ${str}`
-        searchSimilarArtist(str)
+  currentWindow: true,
+  active: true
+}, function(tab) {
+  browser.tabs.sendMessage(tab[0].id, {
+    text: ''
+  }).then(function(response) {
+      const title = response.title
+      const str = response.str
+      document.getElementById('title').innerText = `you have selected ${str}`
+      searchSimilarArtist(str)
 
-        }
+    }
 
-    )
+  )
 })
 
-function searchSimilarArtist(artist){
+function searchSimilarArtist(artist) {
   //authentificate
   const auth = new LastFM('6365215872671c325787a220ef38ae1c')
   console.log(artist)
   //return artist similar to given name
-  const data = {isFound:false,result:"ea"}
-  auth.artistSimilar({name:artist},(err,data)=>{
-    if(err){
-      data.isFound=false
-      data.result=err
+  const data = {
+    isFound: false,
+    result: "ea"
+  }
+  auth.artistSimilar({
+    name: artist
+  }, (err, data) => {
+    if (err) {
+      data.isFound = false
+      data.result = err
       console.log(data)
-    }
-    else {
+    } else {
       data.isFound = true
       data.result = data
       console.log(data)
       let html = ""
-      for(var i = 0; i<10; i++){
+      for (var i = 0; i < 10; i++) {
         html += `<br /><div>${data.artist[i].name}</div>`
       }
       console.log(html)
       document.getElementById('artist').innerHTML = html
-      }
-  })}
+    }
+  })
+}
